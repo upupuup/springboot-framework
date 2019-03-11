707da5a8635d9be765d09ef886f21b6fc525b8f0
upupuup <woyaojiayou_a@163.com>
2019-03-11 21:16:53 +0800

    Initial commit

---
+++ src/main/java/com/and/springbootframework/SpringbootFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.and.springbootframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/and/springbootframework/SpringbootFrameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.and.springbootframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootFrameworkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
